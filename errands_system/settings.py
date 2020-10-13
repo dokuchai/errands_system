5d74a60b9c03145a379228da26d66ae638893c27
dokuchai <v3dok94@gmail.com>
2020-10-14 00:45:40 +0300

    Added static root & media root

--- errands_system/settings.py
+++ errands_system/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'djoser',
     'changelog.apps.ChangelogConfig',
     'errands.apps.ErrandsConfig',
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -98,4 +100,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path.joinpath(BASE_DIR, "static")
+STATICFILES_DIRS = ()
 MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, "media")
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+FILE_UPLOAD_PERMISSIONS = 0o757

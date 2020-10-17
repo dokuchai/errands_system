5b9701e8d7f75357a094fe49af5e185c3c422de6
dokuchai <v3dok94@gmail.com>
2020-10-17 16:32:02 +0300

    sdf

--- errands/serializers.py
+++ errands/serializers.py
@@ -1,13 +1,13 @@
 from abc import ABC
 
-from django.db.models import Value, CharField
+from django.db.models import Value, CharField, F
 from rest_framework import serializers
 from .models import Boards, Tasks
 
 
 class TaskListSerializer(serializers.ModelSerializer):
     term = serializers.DateTimeField(input_formats=["%d-%m-%Y", "%Y-%m-%d", "%d.%m.%Y"], required=False)
-    responsible = serializers.CharField(source='responsible.get_full_name')
+    # responsible = serializers.CharField(source='responsible.get_full_name')
 
     class Meta:
         model = Tasks
@@ -16,11 +16,13 @@ class TaskListSerializer(serializers.ModelSerializer):
 
 class TaskDetailSerializer(serializers.ModelSerializer):
     term = serializers.DateTimeField(input_formats=["%d-%m-%Y", "%Y-%m-%d", "%d.%m.%Y"], allow_null=True)
-    responsible = serializers.CharField(source='responsible.get_full_name')
+    # responsible = serializers.CharField(source='responsible.get_full_name')
 
     class Meta:
         model = Tasks
         fields = ('id', 'title', 'text', 'project', 'term', 'responsible', 'icon', 'status')
+
+    # def responsible_name(self):
 
 
 class BoardSerializer(serializers.ModelSerializer):
@@ -41,5 +43,5 @@ class BoardBaseSerializer(serializers.BaseSerializer, ABC):
             "title": instance.title,
             "projects": [project for project in projects],
             "tasks": Tasks.objects.filter(board=instance, project="").values('id', 'title', 'status', 'term',
-                                                                             'icon', 'board', 'responsible'),
+                                                                             'icon', 'board', 'responsible')
         }

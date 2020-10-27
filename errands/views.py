f0519f83f65c3eef0ebf3e55cd59c7dc332eb31f
dokuchai <v3dok94@gmail.com>
2020-10-27 16:21:29 +0300

    Add year to project

--- errands/views.py
+++ errands/views.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 from rest_framework import viewsets, status
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -137,8 +138,9 @@ class ISUView(APIView):
         board = Boards.objects.get(id=pk)
         tasks = []
         month = str(request.data['date']).split('-')[1]
+        year = datetime.datetime.today().year
         if death_escalation:
-            project, proj_created = Project.objects.get_or_create(title=f'ИСУ {get_month(month)}')
+            project, proj_created = Project.objects.get_or_create(title=f'ИСУ {get_month(month)},{year}')
             recipients_list = set([task_isu['recipient'] for task_isu in death_escalation])
             recipient_set = (set(map(lambda x: board.owner.position.lower().startswith(x.lower()), recipients_list)))
             if True not in recipient_set:

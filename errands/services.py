eda43e1e967159a0110c7c3e7595776d08bd4bb6
dokuchai <v3dok94@gmail.com>
2020-11-02 02:09:56 +0300

    Fix

--- errands/services.py
+++ errands/services.py
@@ -27,16 +27,15 @@ def add_new_responsible(first_name, last_name, board_id):
     return user
 
 
-def split_task_message(title, message):
-    message_dict = {"icon": None, "message": f'{title}. {message}'}
+def split_task_message(message):
+    message_dict = {"icon": None, "message": message}
     if str(message).startswith('ИСУ'):
         icon_list = str(message).split(' ')[:2]
         message_list = str(message).split(' ')[2:]
         icon_title = ' '.join(icon_list)
         icon = Icons.objects.get(description__icontains=icon_title)
         split_message = ' '.join(message_list)
-        title_with_split_message = f'{title}. {split_message}'
-        message_dict.update({"icon": icon.id, "message": title_with_split_message})
+        message_dict.update({"icon": icon.id, "message": split_message})
     return message_dict
 
 
@@ -45,7 +44,7 @@ def get_or_create_isu_tasks(items, board, responsible_full_name, tasks, project,
         [f'{item["last_name"]} {item["first_name"]} {item["father_name"]}' for item in items])
     if f'{board.owner.last_name} {board.owner.first_name} {board.owner.father_name}' in recipients_fio_set:
         for item in items:
-            message_and_icon = split_task_message(title=item['title'], message=item['message'])
+            message_and_icon = split_task_message(message=item['message'])
             full_name = f"{item['last_name']} {item['first_name']} {item['father_name']}"
             if responsible_full_name == full_name:
                 task, created = Tasks.objects.get_or_create(title=message_and_icon['message'], board=board,
--- errands/views.py
+++ errands/views.py
@@ -140,8 +140,7 @@ class ISUView(APIView):
         tasks = []
         project_death, project_death_created = Project.objects.get_or_create(title='Смертность')
         project_ambulance, project_ambulance_created = Project.objects.get_or_create(title='Скорая')
-        responsible = CustomUser.objects.get(first_name=board.owner.first_name,
-                                             last_name=board.owner.last_name,
+        responsible = CustomUser.objects.get(first_name=board.owner.first_name, last_name=board.owner.last_name,
                                              father_name=board.owner.father_name, boards=board)
         responsible_full_name = f"{responsible.last_name} {responsible.first_name} {responsible.father_name}"
         if ambulances:
@@ -154,6 +153,6 @@ class ISUView(APIView):
             get_or_create_isu_tasks(items=test_output, board=board, responsible_full_name=responsible_full_name,
                                     tasks=tasks, project=project_death, responsible=responsible)
         if tasks:
-            return Response({"message": f"Найдено {len(tasks)} задач!"}, status=status.HTTP_201_CREATED)
+            return Response(TaskListSerializer(tasks, many=True).data, status=status.HTTP_201_CREATED)
         else:
             return Response({"message": "Задач не найдено!"}, status=status.HTTP_200_OK)

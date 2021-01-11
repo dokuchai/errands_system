5a59b2060917fc654d847d15f853fa8e3d119986
dokuchai <v3dok94@gmail.com>
2021-01-11 12:42:23 +0300

    try fix

--- changelog/mixins.py
+++ changelog/mixins.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.options import make_immutable_fields_list
 
 
 class ChangeloggableMixin(models.Model):
@@ -15,20 +16,53 @@ class ChangeloggableMixin(models.Model):
             field.name: getattr(self, field.name)
             for field in self._meta.fields if field.name not in ['added', 'changed'] and hasattr(self, field.name)
         }
-        self._original_values.update({
-            field.name: getattr(self, field.name)
-            for field in self._meta.many_to_many if hasattr(self, field.name) and field.name not in ['added', 'changed']
-        })
+        # self._original_values.update({
+        #     field.name: getattr(self, field.name)
+        #     for field in self._meta.many_to_many if hasattr(self, field.name)
+        # })
+        # self._state.fields_cache.update({
+        #     field.name: getattr(self, field.name).all()
+        #     for field in self._meta.many_to_many if hasattr(self, field.name)
+        # })
+        # self.__dict__.update({
+        #     field.name: getattr(self, field.name).all()
+        #     for field in self._meta.many_to_many if hasattr(self, field.name)
+        # })
 
     def get_changed_fields(self):
         """
         Получаем измененные данные
         """
         result = {}
-        result.update({name: getattr(self, name) for name, value in self._original_values.items() if
-                       value != getattr(self, name)})
-        result.update(
-            {'so_executors': [so_executor for so_executor in self._original_values.get('so_executors').all() if
-                              'so_executors' in self._original_values.keys()]})
-        print(result.get('so_executors'))
+        print(self.__dict__.keys())
+        for name, value in self._original_values.items():
+            # print(name, value, value != getattr(self, name))
+            if value != getattr(self, name):
+                print(value, getattr(self, name))
+                temp = {}
+                temp[name] = getattr(self, name)
+                result.update(temp)
+            # if hasattr(value, 'all') and value.all() != getattr(self, name).all():
+            # print(dir(self._meta.local_many_to_many[0]))
+            # print(value.set.__self__.values('email'))
+            # print(value.all())
+            # print(getattr(self, name).all())
+            # print([_ for _ in value.all()] != [_ for _ in getattr(self, name).all()])
+            # print([obj for obj in value.all()])
+            # print(value.all(), getattr(self, 'so_executors').all())
+            # print(value.all != getattr(self, 'so_executors').all)
+        # result.update({name: getattr(self, name) for name, value in self._original_values.items() if
+        #                value != getattr(self, name)})
+        # result.update(
+        #     {'so_executors': [so_executor for so_executor in self._original_values.get('so_executors').all() if
+        #                       'so_executors' in self._original_values.keys()]})
+        # if getattr(self, 'so_executors').all():
+        #     # print(self._original_values.items())
+        #     # print(self._original_values.get('so_executors').all())
+        #     # print(getattr(self, 'so_executors').all())
+        #     temp = {}
+        #     so_executors = [so_executor for so_executor in getattr(self, 'so_executors').all()]
+        #     temp['so_executors'] = so_executors
+        #     result.update(temp)
+        # print(result.get('so_executors'))
         return result

7bc522026b01810b1ee416e4f6c11205ccff0410
dokuchai <v3dok94@gmail.com>
2021-01-12 13:16:41 +0300

    Fix request user in singleton

--- changelog/middleware.py
+++ changelog/middleware.py
@@ -1,3 +1,9 @@
+from rest_framework.authentication import RemoteUserAuthentication
+
+from errands_system import settings
+from users.authentication import ExpiringTokenAuthentication
+
+
 class Singleton(object):
     """Синглтон"""
 
@@ -18,8 +24,14 @@ class LoggedInUser(Singleton):
     def set_data(self, request):
         self.request = id(request)
         if request.user.is_authenticated:
-            self.user = request.user
             self.address = request.META.get('REMOTE_ADDR')
+
+    def set_user(self, request):
+        try:
+            request.user = ExpiringTokenAuthentication().authenticate(request=request).__getitem__(0)
+            self.user = request.user
+        except AttributeError:
+            self.user = request.user
 
     @property
     def current_user(self):
@@ -40,6 +52,7 @@ class LoggedInUserMiddleware(object):
         """
         logged_in_user = LoggedInUser()
         logged_in_user.set_data(request)
+        logged_in_user.set_user(request)
         response = self.get_response(request)
 
         return response

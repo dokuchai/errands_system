0a8fe454fb480c2d16172c7dac535af937576c78
dokuchai <v3dok94@gmail.com>
2021-01-19 12:48:43 +0300

    Remove excess fields from task list search serializer

--- errands/serializers.py
+++ errands/serializers.py
@@ -138,8 +138,7 @@ class TaskListSearchSerializer(TaskListSerializer):
     class Meta:
         model = Tasks
         fields = (
-            'id', 'title', 'status', 'term', 'project', 'responsible', 'icon', 'icon_url', 'board', 'board_id',
-            'resp_id', 'version', 'user_status')
+            'id', 'title', 'project', 'board', 'board_id', 'user_status')
 
     def get_user_status(self, obj):
         friend = FriendBoardPermission.objects.get(board_id=obj.board.id, friend_id=self.context['user'])
